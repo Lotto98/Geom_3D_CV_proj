d02ace3e763f063738987d5e2f6c57ac999dc5a2
Lotto98 <lottomichele98@gmail.com>
2025-01-17 12:08:19 +0100

    bugfix download RBF interpolation

--- download_RBF_interpolation.sh
+++ download_RBF_interpolation.sh
@@ -1,3 +1,7 @@
+#create the folder if it does not exist
+mkdir -p ~/Documents/Geom_3D_CV_proj/results/RBF
+
+#download the files
 scp michele@lottohouse.duckdns.org:/home/michele/Documents/Geom_3D_CV_proj/results/RBF/coin1.npz ~/Documents/Geom_3D_CV_proj/results/RBF/coin1.npz
 scp michele@lottohouse.duckdns.org:/home/michele/Documents/Geom_3D_CV_proj/results/RBF/coin2.npz ~/Documents/Geom_3D_CV_proj/results/RBF/coin2.npz
 scp michele@lottohouse.duckdns.org:/home/michele/Documents/Geom_3D_CV_proj/results/RBF/coin3.npz ~/Documents/Geom_3D_CV_proj/results/RBF/coin3.npz
