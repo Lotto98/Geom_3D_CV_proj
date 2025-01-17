258c660f985cb4652b90c625ff67fe0c7f77a3a0
Michele <lottomichele98@gmail.com>
2025-01-17 11:47:21 +0100

    added utility scripts

---
+++ RBF_interpolation.sh
@@ -0,0 +1,4 @@
+python3 src/analysis.py --coin 1 --interpolate --method RBF --nprocesses -1
+python3 src/analysis.py --coin 2 --interpolate --method RBF --nprocesses -1
+python3 src/analysis.py --coin 3 --interpolate --method RBF --nprocesses -1   
+python3 src/analysis.py --coin 4 --interpolate --method RBF --nprocesses -1      
---
+++ download_RBF_interpolation.sh
@@ -0,0 +1,4 @@
+scp michele@lottohouse.duckdns.org:/home/michele/Documents/Geom_3D_CV_proj/results/RBF/coin1.npz ~/Documents/Geom_3D_CV_proj/results/RBF/coin1.npz
+scp michele@lottohouse.duckdns.org:/home/michele/Documents/Geom_3D_CV_proj/results/RBF/coin2.npz ~/Documents/Geom_3D_CV_proj/results/RBF/coin2.npz
+scp michele@lottohouse.duckdns.org:/home/michele/Documents/Geom_3D_CV_proj/results/RBF/coin3.npz ~/Documents/Geom_3D_CV_proj/results/RBF/coin3.npz
+scp michele@lottohouse.duckdns.org:/home/michele/Documents/Geom_3D_CV_proj/results/RBF/coin4.npz ~/Documents/Geom_3D_CV_proj/results/RBF/coin4.npz
