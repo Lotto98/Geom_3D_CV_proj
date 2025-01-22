e322deceb7b0ee1a1017388e8b5dbd3075cdbe27
Lotto98 <lottomichele98@gmail.com>
2025-01-22 18:10:17 +0100

    bugfix on download script and added compte light on PTM_interpolation

--- download_RBF_interpolation.sh
+++ download_RBF_interpolation.sh
@@ -1,7 +1,6 @@
 #create the folder if it does not exist
-mkdir -p ~/Documents/Geom_3D_CV_proj/results/RBF
-mkdir -p ~/Documents/Geom_3D_CV_proj/results/RBF_cuda
+mkdir -p ~/Documents/Geom_3D_CV_proj/results/
 
 #download the files
-scp -r michele@lottohouse.duckdns.org:/home/michele/Documents/Geom_3D_CV_proj/results/RBF ~/Documents/Geom_3D_CV_proj/results/RBF
-scp -r michele@lottohouse.duckdns.org:/home/michele/Documents/Geom_3D_CV_proj/results/RBF_cuda ~/Documents/Geom_3D_CV_proj/results/RBF_cuda
+scp -r michele@lottohouse.duckdns.org:/home/michele/Documents/Geom_3D_CV_proj/results/RBF ~/Documents/Geom_3D_CV_proj/results
+scp -r michele@lottohouse.duckdns.org:/home/michele/Documents/Geom_3D_CV_proj/results/RBF_cuda ~/Documents/Geom_3D_CV_proj/results
